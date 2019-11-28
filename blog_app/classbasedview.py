4e9c4c25e44ef9dc048a0eb9edc3afb199286b67
rohit <rohityadav01111@gmail.com>
2019-11-28 12:05:05 +0530

    api view

---
+++ blog_app/classbasedview.py
@@ -0,0 +1,135 @@
+from django.contrib.auth.models import User
+from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
+from rest_framework.settings import api_settings
+
+from .permissions import IsOwnerOrReadOnly
+from .models import BlogPost
+from .serializers import BlogPostSerializer, UserPostSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, generics, permissions
+
+#
+# class PostList(APIView, LimitOffsetPagination):
+#     """
+#     List all snippets, or create a new snippet.
+#     """
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+#
+#     def get(self, request):
+#         snippets = BlogPost.objects.all()
+#         paginated_data = self.paginate_queryset(snippets, request)
+#         serializer = BlogPostSerializer(paginated_data, many=True, context={'request': request})
+#         return Response(serializer.data)
+#
+#     def post(self, request):
+#         serializer = BlogPostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             BlogPost.author_id = request.user_id
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def perform_create(self, serializer):
+#         serializer.save(author=self.request.user)
+#
+#
+# class PostDetail(APIView):
+#     """
+#     Retrieve, update or delete a snippet instance.
+#     """
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+#
+#     def get_object(self, pk):
+#         try:
+#             return BlogPost.objects.get(pk=pk)
+#         except BlogPost.DoesNotExist:
+#             raise Http404
+#
+#     def get(self, request, pk, format=None):
+#         posts = self.get_object(pk)
+#         serializer = BlogPostSerializer(posts)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         posts = self.get_object(pk)
+#         serializer = BlogPostSerializer(posts, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         snippet.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
+
+
+class BlogList(APIView, LimitOffsetPagination):
+    """
+    List all snippets, or create a new snippet.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get(self, request):
+        pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
+        paginator = pagination_class()
+        queryset = BlogPost.objects.all()
+        page = paginator.paginate_queryset(queryset, request)
+        # paginated_data = self.paginate_queryset(snippets, request)
+        serializer = BlogPostSerializer(page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BlogPostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class BlogDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    permission_classes = [permissions.IsAuthenticated, ]
+
+    def get_object(self, pk):
+        try:
+            return BlogPost.objects.get(pk=pk)
+        except BlogPost.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        blog = self.get_object(pk)
+        serializer = BlogPostSerializer(blog)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        blog = self.get_object(pk)
+        serializer = BlogPostSerializer(blog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        blog = self.get_object(pk)
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserPostSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserPostSerializer
+
--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,12 +1,17 @@
 """ django urls are checked here"""
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
-from .accounts import signup, login
-from .views import post_list, post_details
+from .classbasedview import BlogList, BlogDetail, UserDetail, UserList
 
 urlpatterns = [
-    path("post", post_list, name='post_list'),
-    path('post/<int:pk>/', post_details, name='post-detail'),
-    path('signup/', signup, name='SignupView'),
-    path('login/', login, name='loginView'),
+    path("blog/", BlogList.as_view(), name='blog_list'),
+    path('blog/<int:pk>/', BlogDetail.as_view(), name='blog_detail'),
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()),
+    # path("post/", PostList.as_view(), name='post_list'),
+    # path('post/<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    # path('signup/', signup, name='SignupView'),
+    # path('login/', login, name='loginView'),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
